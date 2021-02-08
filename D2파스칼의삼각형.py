2dfd1804e63112a1f62c203eda922de0fdedb9b2
wjseong0704 <57784685+wjseong0704@users.noreply.github.com>
2021-02-08 20:12:14 +0900

    Update D2파스칼의삼각형.py

--- D2파스칼의삼각형.py
+++ D2파스칼의삼각형.py
@@ -1,10 +1,17 @@
-def pascal(list a):
-    i = 0
-    for i in range(1,len(a)):
-        b[i] = a[i-1] + a[i]
-
 T = int(input())
 for test_case in range(1,T+1):
     w = int(input())
-    a = [1,1,1]
-    for i in range(w):
+    a = [[1]] 
+    for i in range(1, w):
+        b = []
+        b.append(1)
+        for j in range(i-1):
+            b.append(0)
+            b[j+1] = a[i-1][j] + a[i-1][j+1]
+        b.append(1)
+        a.append(b)
+    print(f"#{test_case}")
+    for i in a:
+        for j in i:
+            print(j, end=' ')
+        print()
