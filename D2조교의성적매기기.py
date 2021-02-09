d7cdd231ddbd0288f9d41e0edb1b289df0d6a658
wjseong0704 <57784685+wjseong0704@users.noreply.github.com>
2021-02-09 20:16:45 +0900

    Update D2조교의성적매기기.py

--- D2조교의성적매기기.py
+++ D2조교의성적매기기.py
@@ -1,4 +1,8 @@
-T = int(input())
-for test_case in range(1,T+1):
-    
-    M, F, H = list(map(int, input().split(" ")))
+#T = int(input())
+#for test_case in range(1,T+1):
+N = int(input())
+student = []
+for i in range(N):
+    score = list(map(int, input().split(" ")))
+    student.append(score)
+print(student)
