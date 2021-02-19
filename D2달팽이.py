de1fc1b182e474f2b35853d34be605a91a994f79
wjseong0704 <57784685+wjseong0704@users.noreply.github.com>
2021-02-19 18:33:02 +0900

    Create D2달팽이.py

---
+++ D2달팽이.py
@@ -0,0 +1,16 @@
+#T = int(input())
+#for test_case in range(1,T+1):
+N = int(input())
+square = []
+inner = []
+for i in range(N):
+    inner.append(0)
+    square.append(inner)
+print(square)
+#초기값 설정
+if N%2 == 0:
+    square[N//2][(N//2)-1] = 
+else:
+
+#뒤에서 부터 번호 찍기
+#
