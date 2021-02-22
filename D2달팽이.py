0377f5af791c892a0040375639831104fca4e3af
wjseong0704 <57784685+wjseong0704@users.noreply.github.com>
2021-02-22 19:25:31 +0900

    Update D2달팽이.py

--- D2달팽이.py
+++ D2달팽이.py
@@ -1,16 +1,57 @@
+def left(i,j):
+    i = i
+    j = j-1
+    return i,j
+def right(i,j):
+    i = i
+    j = j+1
+    return i,j
+def up(i,j):
+    i = i-1
+    j = j
+    return i,j
+def down(i,j):
+    i = i+1
+    j = j
+    return i,j
+
 #T = int(input())
 #for test_case in range(1,T+1):
+#사각형만들기
 N = int(input())
-square = []
-inner = []
-for i in range(N):
-    inner.append(0)
-    square.append(inner)
+square = [[0] * N for s in range(N)]
+#inner = [0] * N
+#for i in range(N):
+#    inner.append(i)
+#    square.append(inner)
 print(square)
 #초기값 설정
 if N%2 == 0:
-    square[N//2][(N//2)-1] = 
+    a = N//2
+    b = N//2 -1
+    square[a][b] = N*N
 else:
+    a = N//2
+    b = N//2
+    square[a][b] = N*N
+    flag = True
+print(square)
+#길 뚫기
+#test = [[1,2,3],[4,5,6],[7,8,9]]
+w = []
+for i in range(N*N):
+    w.append(i)
+w.reverse()
+print(w)
 
-#뒤에서 부터 번호 찍기
-#
+for i in range(N):
+    if a == 0 or b == 0:
+        flag = False
+   
+for i in range(1,N):
+    if flag:
+        a = a 
+        b = b + (-1)*flag
+        print(a,b)
+    else:
+        a = a
